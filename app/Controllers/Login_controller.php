bee732eec8e302a6344f533d2bd526c375c48c79
Jeremias Gonzalez <jeremiasgonzalez7464@gmail.com>
2025-06-28 01:32:12 -0300

    update login controller

--- app/Controllers/Login_controller.php
+++ app/Controllers/Login_controller.php
@@ -10,7 +10,7 @@ class Login_controller extends Controller{
          $dato['titulo']='Ingreso'; 
          echo view('front/head_view',$dato);
          echo view('front/nav_view');
-         echo view('front/login');
+         echo view('back/usuario/login');
          echo view('front/footer_view');
     }
 
