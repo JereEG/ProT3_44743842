c6501a3882ac6d19f56ebddf15d58343110ade00
Jeremias Gonzalez <jeremiasgonzalez7464@gmail.com>
2025-06-28 01:29:06 -0300

    configuracion de base de datos

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
+        'username'     => 'root',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'gonzalez_jeremias_ezequiel',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
